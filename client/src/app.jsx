f82af59b7a33ac48765e739c06c0af2ced195ac2
Jesus Rivera <r.jesus9107@gmail.com>
2023-04-16 15:05:24 -0700

    [update] main component to include the column, feature, and slider components

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { fetchTopHeadlines } from './api'
 
+import Featured from './components/featured'
+import Slider from './components/slider'
+import Column from './components/column'
+
 const App = () => {
   const [articles, setArticles] = useState([])
+  const [sliderArticles, setSliderArticles] = useState([])
+  const [featuredArticle, setFeaturedArticle] = useState({})
+  const [columnArticles, setColumnArticles] = useState([])
+  const [loading, setLoading] = useState(true)
 
   const getTopHeadlines = async () => {
     const headlines = await fetchTopHeadlines()
@@ -16,8 +24,30 @@ const App = () => {
     }
   }, [])
 
+  useEffect(() => {
+    if (articles.length) {
+      setFeaturedArticle(articles[Math.floor(Math.random() * articles.length)])
+      setSliderArticles(articles.slice(0, 5))
+      setColumnArticles(articles.slice(5, 10))
+    }
+    setLoading(false)
+  }, [articles])
+
+  if (loading) return <h1>Loading...</h1>
+
   return (
     <>
+      <header>
+        <i className="fa-solid fa-newspaper"></i>
+        <h2>News Digest</h2>
+
+        <button onClick={getTopHeadlines}>Log in</button>
+      </header>
+      <div id="container">
+        <Column articles={columnArticles} />
+        <Featured article={featuredArticle} />
+        <Slider articles={sliderArticles} />
+      </div>
     </>
   )
 }
